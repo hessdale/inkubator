1d56a00af0b035b205f90307f3aabb3fcf667bf2
hessdale <hessdale96@gmail.com>
2023-11-13 11:51:18 -0800

    added mission statements component with rough info and color schemes

---
+++ src/components/MissionStatements.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div id="started">
+            <h1 class="title">How It Started</h1>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae obcaecati consequatur harum
+                et cum. Ab,
+                itaque atque! Fugit voluptate voluptates repellat nobis veritatis aliquam excepturi, quidem corrupti saepe,
+                minima vitae?</p>
+            <div class="block"></div>
+        </div>
+        <div id="now">
+            <h1 class="title">Where It Is Now</h1>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, error fugiat aliquid nam
+                reprehenderit,
+                distinctio perferendis neque nulla voluptatum nobis, hic aperiam ducimus amet est inventore itaque beatae
+                doloribus? Quae?</p>
+            <div class="block"></div>
+        </div>
+        <div id="become">
+            <h1 class="title">What It Will Become</h1>
+            <p class="info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni, fugit. Quaerat impedit eveniet
+                dolorum,
+                quidem repellat praesentium placeat veritatis nulla optio soluta maiores doloremque aspernatur tempore
+                suscipit nihil ratione. Error.</p>
+            <div class="block"></div>
+        </div>
+        <div id="why">
+            <h1 class="title">Why We Need You</h1>
+            <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex deleniti rem reiciendis eligendi
+                veniam, nulla ullam ducimus! Iusto quas veritatis suscipit magni consequatur, nostrum eius asperiores rerum,
+                recusandae quod iste.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url(https://fonts.googleapis.com/css?family=Jost:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic);
+@import url(https://fonts.googleapis.com/css?family=Baskervville:regular,italic);
+
+.why {
+    background-color: #682138;
+}
+
+.info {
+    font-family: 'Baskervville', sans-serif;
+}
+
+.title {
+    font-family: 'Jost', sans-serif;
+}
+
+.block {
+    background-color: #ed1557;
+    width: 200px;
+    height: 200px;
+}
+</style>
